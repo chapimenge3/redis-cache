edf09e9ce2e0d62ddfed31423fc327ebec06fba7
chapi <chapimenge3@gmail.com>
2022-07-27 14:43:17 +0300

    config static file

--- caching/caching/urls.py
+++ caching/caching/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('event.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
-
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
